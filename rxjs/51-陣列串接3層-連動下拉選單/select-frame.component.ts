ec6424b8417c61304507498ba5d1a5d83a8e179b
jscode1972 <38292163+jscode1972@users.noreply.github.com>
2023-05-28 08:56:22 +0800

    Create select-frame.component.ts

---
+++ rxjs/51-陣列串接3層-連動下拉選單/select-frame.component.ts
@@ -0,0 +1,123 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Subject, zip, switchMap, Observable, of, from, tap, filter, map, distinct, toArray } from 'rxjs';
+import { Area, Nation, City, Full } from '../models';
+import { FrameService } from '../frame.service';
+
+@Component({
+  selector: 'app-select-frame',
+  templateUrl: './select-frame.component.html',
+  styleUrls: ['./select-frame.component.css']
+})
+export class SelectFrameComponent implements OnInit {
+  @Input() aid : string = '';
+  @Input() allowAll : boolean = false;
+  //
+  areas!: Area[];     // 洲
+  nations!: Nation[]; // 國家
+  cities!: City[];    // 城市
+  fulls!: Full[];     // 合併三層
+  alias!: string[];   // 模擬 ClassMain, ClassSub
+  // 下拉選單推送物件
+  areaSource$ = new Subject<string>();
+  nationSource$ = new Subject<string>();
+  citySource$ = new Subject<string>();
+  // 查詢條件
+  form : FormGroup =  this.fb.group({
+    aid: this.fb.control('', [Validators.required]),
+    nation: this.fb.control('', [Validators.required]),
+    cid: this.fb.control('', [Validators.required]),
+  });
+
+  constructor(public fb: FormBuilder,
+              private svc: FrameService) {
+  }
+
+  ngOnInit() {
+    this.bindEvent();
+    this.zipArray();
+  }
+
+  zipArray() {
+    // 需三個一起才完成  原本要呼叫三次 (需全部完成再開始)
+    zip( 
+      this.svc.getAreas(),
+      this.svc.getCountry(),
+      this.svc.getCity(),
+      this.svc.getFull()
+    ).subscribe((arr) => {
+      this.areas = arr[0];   // continent
+      this.nations = arr[1]; // nation 
+      this.cities = arr[2];  // city
+      this.fulls = arr[3];  
+      // 預設最上層下拉選項
+      let aid = this.aid ? this.aid : this.areas[0].aid;
+      this.form.patchValue({ aid: aid}); // 這一行很重要
+    })
+  }
+
+  bindEvent() {
+    // 洲別選項推播
+    this.areaSource$
+      .pipe(
+        switchMap( aid => this.prepareNationList(aid) ),
+        switchMap( arr => this.assignNationDefault(arr) ) 
+      )
+      .subscribe( (val) => {
+        // 如果只要預設 area, 但不預設下一層, 此行拿掉即可
+        this.form.get("nation")?.setValue(val);
+      });
+    // 國家選項推播
+    this.nationSource$
+      .pipe(
+        tap((x) => console.log(x) ),
+        switchMap((nation) => this.prepareCity(nation)),
+        switchMap( arr => this.assignCityDefault(arr) )
+      )
+      .subscribe( (val) => {
+        // 如果只要預設 area, 但不預設下一層, 此行拿掉即可
+        this.form.get("cid")?.setValue(val);
+      });
+  }
+
+  prepareNationList(aid : string) : Observable<string[]> {
+    // 回傳國家下拉選單
+    return from(this.fulls).pipe(
+      filter((area) => (area.aid === aid) || ('ALL' === aid) ),
+      map((o) => o.nation),
+      distinct(),
+      toArray(),
+      map((arr) => arr.sort((x,y) => x<y ? -1 : 1 )),
+      tap((arr) => { this.alias = arr; })
+    );
+  }
+
+  assignNationDefault(arr : string[]) : Observable<string|null> {
+    // 是否開啟國家預設選項
+    let nation = this.allowAll ? 'ALL' : (arr.length>0 ? arr[0] : null );
+    return of(nation).pipe(
+      // 清除下層設定, 這一行很重要
+      tap(() => this.form.patchValue({nation: null, cid: null }) ) 
+    );
+  }
+
+  prepareCity(nation : string) : Observable<any> {
+    // 回傳下拉選單
+    return from(this.fulls).pipe(
+      filter((arr) => arr.aid === this.form.get("aid")?.value ),
+      filter((arr) => (arr.nation === nation) || ('ALL' === nation) ),
+      toArray(),
+      map((arr) => arr.sort((x,y) => x.city<y.city ? -1 : 1 )),
+      tap((arr) => { this.cities = arr; })
+    );
+  }
+
+  assignCityDefault(arr : Full[]) : Observable<string|null> {
+    // 是否開啟城市預設選項
+    let cid = this.allowAll ? 'ALL' : (arr.length>0 ? arr[0].cid : null );
+    return of(cid).pipe(
+      // 清除下層設定, 這一行很重要
+      tap(() => this.form.patchValue({ cid: null })) 
+    );
+  }
+}
