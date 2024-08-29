a5db266cb38f3545937a539a2dfa763de9392afe
jscode1972 <38292163+jscode1972@users.noreply.github.com>
2024-08-30 02:42:40 +0800

    Update and rename run.py to .py

--- 機器學習-DEMO/WeatherForecaset/.py
+++ 機器學習-DEMO/WeatherForecaset/.py
@@ -7,7 +7,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import keras
 from keras.utils import timeseries_dataset_from_array, timeseries_dataset_from_array
-from ClimateData import titles, feature_keys, date_time_key # show_raw_visualization
 
 # 使用範例
 uri = "https://storage.googleapis.com/tensorflow/tf-keras-datasets/jena_climate_2009_2016.csv.zip"
@@ -35,6 +34,76 @@ def load_data(csv_fname):
 #df = pd.download_and_extract_data(uri, zip_fname, csv_fname)
 df = load_data(csv_fname)
 print(df.shape) # (420551, 15)
+
+
+titles = [
+    "Pressure",
+    "Temperature",
+    "Temperature in Kelvin",
+    "Temperature (dew point)",
+    "Relative Humidity",
+    "Saturation vapor pressure",
+    "Vapor pressure",
+    "Vapor pressure deficit",
+    "Specific humidity",
+    "Water vapor concentration",
+    "Airtight",
+    "Wind speed",
+    "Maximum wind speed",
+    "Wind direction in degrees",
+]
+
+feature_keys = [
+    "p (mbar)",
+    "T (degC)",
+    "Tpot (K)",
+    "Tdew (degC)",
+    "rh (%)",
+    "VPmax (mbar)",
+    "VPact (mbar)",
+    "VPdef (mbar)",
+    "sh (g/kg)",
+    "H2OC (mmol/mol)",
+    "rho (g/m**3)",
+    "wv (m/s)",
+    "max. wv (m/s)",
+    "wd (deg)",
+]
+
+colors = [
+    "blue",
+    "orange",
+    "green",
+    "red",
+    "purple",
+    "brown",
+    "pink",
+    "gray",
+    "olive",
+    "cyan",
+]
+
+date_time_key = "Date Time"
+
+def show_raw_visualization(data):
+    time_data = data[date_time_key]
+    fig, axes = plt.subplots(
+        nrows=7, ncols=2, figsize=(15, 20), dpi=80, facecolor="w", edgecolor="k"
+    )
+    for i in range(len(feature_keys)):
+        key = feature_keys[i]
+        c = colors[i % (len(colors))]
+        t_data = data[key]
+        t_data.index = time_data
+        t_data.head()
+        ax = t_data.plot(
+            ax=axes[i // 2, i % 2],
+            color=c,
+            title="{} - {}".format(titles[i], key),
+            rot=25,
+        )
+        ax.legend([titles[i]])
+    plt.tight_layout()
 
 # Raw Data Visualization (顯示資料分布)
 # show_raw_visualization(df)
